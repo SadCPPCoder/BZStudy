24235265a8acd0c6bc843e37e813fbe6eb543e8c
”SadCPPCoder” <156500117@qq.com>
2020-03-22 13:55:13 +0000

    Practice the async() API
    Practice async() and record the study note.

--- cpp_program/concurrent_programming/async_demo.cpp
+++ cpp_program/concurrent_programming/async_demo.cpp
@@ -52,7 +52,61 @@ int async_demo0()
     std::cout << "\nresult of func1()+func2(): " << result << endl;
 }
 
+int do_something_ver_1(int type)
+{
+	cout << __func__ << endl;
+
+	if(type<10)
+    {
+    	cout << "\tthrow a double type exception\n";
+		throw 1.6;
+    }
+	else if(type>100)
+    {
+	    cout << "\tthrow a string type exception\n";
+		throw "hello";
+    }
+
+    cout << "\tcalculate finished!\n";
+    return 1;
+}
+
+void async_demo1()
+{
+    auto f1 = async(launch::async, do_something_ver_1, 9);
+    auto f2 = async(launch::async, do_something_ver_1, 999);
+    auto f3 = async(launch::async, do_something_ver_1, 99);
+
+    try
+    {
+        f1.get(); // capture the double exception
+        f2.get(); // throw string exception
+        f3.get(); // run well
+    }
+    catch(const double& err)
+    {
+        cout << "catch a double exception!\n";
+    }
+    catch(...)
+    {
+        cout << "oops! catch a unknown exception.\n";
+    }
+}
+
+void async_demo2(bool flag)
+{
+    auto f1 = async(launch::deferred, doSomething, '*');
+    auto f2 = async(launch::deferred, doSomething, '+');
+
+    auto result = flag ? f1.get() : f2.get();
+
+    cout << "lazy evaluation result: " << result << ".\n";
+}
+
 int main()
 {
-    async_demo0();
+    //async_demo0();
+    //async_demo1();
+    async_demo2(true);
+    async_demo2(false);
 }
